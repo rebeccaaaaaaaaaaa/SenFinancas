6e53c1ab68b2bea2b8915d3787dc5efe1b43b509
rebeccaaaaaaaaaaa <rebeccaignacio1996@gmail.com>
2023-08-09 19:56:27 -0300

    feat: create filters

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -5,9 +5,39 @@ import { useGlobal } from "@/src/hooks/useGlobal";
 
 export function TableContent() {
   const { removeTransaction, transactions } = useGlobal();
+  const [filterType, setFilterType] = useState("all");
+  const [filterCategory, setFilterCategory] = useState("all"); // Estado para controlar a categoria do filtro
+
+  const filteredTransactions = transactions.filter((item: { type: string; category: string; }) => {
+    if (filterType === "all" || item.type === filterType) {
+      // Se o tipo corresponder ou se todos forem selecionados, verifique a categoria
+      return filterCategory === "all" || item.category === filterCategory;
+    }
+    return false;
+  });
+
+  const uniqueCategories = [...new Set(transactions.map((item: { category: string }) => item.category))]; // Obtém categorias únicas
 
   return (
     <div className={styles.container}>
+      <div className={styles.filterButtons}>
+        <button onClick={() => setFilterType("all")}>Todos</button>
+        <button onClick={() => setFilterType("deposit")}>Entrada</button>
+        <button onClick={() => setFilterType("withdraw")}>Saída</button>
+      </div>
+      <div className={styles.filterCategories}>
+      <select
+        value={filterCategory}
+        onChange={event => setFilterCategory(event.target.value)}
+      >
+        <option value="all">Todas as categorias</option>
+        {uniqueCategories.map((category: any, index) => (
+          <option key={index} value={category}>
+            {category}
+          </option>
+        ))}
+      </select>
+      </div>
       <table className={styles.table}>
         <thead>
           <tr className={styles.trHead}>
@@ -19,7 +49,7 @@ export function TableContent() {
           </tr>
         </thead>
         <tbody>
-          {transactions.map((item: any, index: number) => (
+          {filteredTransactions.map((item: any, index: number) => (
             <tr className={styles.trBody} key={index}>
               <td className={styles.pl_16}>{item.title}</td>
               <td
@@ -47,3 +77,4 @@ export function TableContent() {
     </div>
   );
 }
+
